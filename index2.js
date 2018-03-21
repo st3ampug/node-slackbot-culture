8c43517bcbcb6132f6d382f48dd44b1c80018680
Balint Polgar <b.polgar@waracle.com>
2018-03-21 16:13:08 +0000

    added express for routing

--- index2.js
+++ index2.js
@@ -1,7 +1,14 @@
-var slackbot = require('./helpers/bot'); // import slackbot library
-var http = require('http');
+//var slackbot = require('./helpers/bot'); // import slackbot library
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 
+var router = require('./helpers/routes');
+app.use('/', router);
 
-http.createServer((req, res) => {}).listen(1337, '127.0.0.1');
+//http.createServer((req, res) => {}).listen(90, '127.0.0.1');
+app.listen(port, function() {
+    console.log('app started');
+  });
 
-slackbot.run();
+//slackbot.run();
