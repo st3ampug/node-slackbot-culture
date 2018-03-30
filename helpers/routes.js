5923065b439dcb1719576b40ac3e579bca972502
Balint Polgar <b.polgar@waracle.com>
2018-03-30 13:43:34 +0100

    added bot run on pageload

--- helpers/routes.js
+++ helpers/routes.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var slackbot = require('./bot'); // import slackbot library
 
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send('GET request to the page');
+  res.send('GET request for index');
+});
+
+router.get('/startbot', function(req, res) {
+  slackbot.run();
+  res.send('GET request for bot');
 });
 
 module.exports = router;
